2281fa0201b4038433e5454029fefc18fc1e1b56
pleycpl <h4rvey@riseup.net>
2017-01-27 10:50:06 +0000

    added n11.com-lessinfo spider

---
+++ ITbooks/spiders/n11-com.py
@@ -0,0 +1,30 @@
+import scrapy
+# scrapy crawl n11.com out2.json
+
+class N11Spider(scrapy.Spider):
+    name = 'n11.com-lessinfo'
+    allowed_domains = ['n11.com']
+    start_urls = [
+        'http://www.n11.com/kitap/egitim/bilgisayar-kitaplari'
+    ]
+
+    def parse(self, response):
+        items = response.css('.columnContent')
+        for item in items:
+            url = item.css('.plink::attr(href)').extract_first()
+            if url is None:
+                continue
+            title = item.css('.plink::attr(title)').extract_first()
+            img = item.css('.lazy::attr(data-original)').extract_first()
+            old_price = item.css('.oldPrice').xpath('.//del/text()').extract_first()
+            price = item.css('.newPrice').xpath('.//text()').extract_first()
+            saller = item.css('.sallerInfo::attr(title)').extract_first()
+
+            yield {
+                'url':url,
+                'title': title,
+                'img': img,
+                'oldPrice': old_price,
+                'price': price,
+                'saller': saller,
+            }
