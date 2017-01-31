67c4b60044e9ef265651fb57c53416908f8d49e7
h4rvey <h4rvey@protonmail.com>
2017-01-31 20:36:28 +0000

    changed name ITbooks to itbooks

--- ITbooks/settings.py
+++ ITbooks/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for ITbooks project
+# Scrapy settings for itbooks project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -9,14 +9,14 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'ITbooks'
+BOT_NAME = 'itbooks'
 
-SPIDER_MODULES = ['ITbooks.spiders']
-NEWSPIDER_MODULE = 'ITbooks.spiders'
+SPIDER_MODULES = ['itbooks.spiders']
+NEWSPIDER_MODULE = 'itbooks.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'ITbooks (+http://www.yourdomain.com)'
+#USER_AGENT = 'itbooks (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -47,13 +47,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'ITbooks.middlewares.ItbooksSpiderMiddleware': 543,
+#    'itbooks.middlewares.ItbooksSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'ITbooks.middlewares.MyCustomDownloaderMiddleware': 543,
+#    'itbooks.middlewares.MyCustomDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -65,7 +65,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'ITbooks.pipelines.SomePipeline': 300,
+#    'itbooks.pipelines.SomePipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
